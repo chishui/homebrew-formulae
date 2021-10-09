dbfdac319e4ecce870ae2da9bc4510dc62e455bf
Liyun Xiu <chishui2@gmail.com>
2021-10-08 23:25:47 -0700

    Update some information

--- Formula/onewallpaper.rb
+++ Formula/onewallpaper.rb
@@ -1,14 +1,17 @@
 class Onewallpaper < Formula
   desc "Use one image to set continuous wallpaper across monitors"
   homepage "https://github.com/chishui/OneWallpaper"
+  url "https://github.com/chishui/OneWallpaper.git", tag: "1.0.0", revision: "c5ac04849506e768aed5c66f72233fcda2455507"
+  version "0.0.1"
   license "MIT"
-  head "https://github.com/chishui/OneWallpaper.git"
+
+  depends_on :macos
 
   def install
     system "make", "install", "prefix=#{prefix}"
   end
 
   test do
-    system "false"
+    system "#{bin}/one-wallpaper" "import Foundation\n"
   end
 end
